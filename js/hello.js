ce0ef300438c45f78a78e1cc42a114670b8903c4
silviajoy <floriosg@gmail.com>
2019-03-27 18:28:48 +0100

    new class test

---
+++ js/hello.js
@@ -0,0 +1,11 @@
+class Hello {
+
+    constructor(greeting) {
+        this.greeting = greeting + " World!"
+    }
+
+    greet() {
+        console.log(this.greeting)
+        console.log(THREE)
+    }
+}
