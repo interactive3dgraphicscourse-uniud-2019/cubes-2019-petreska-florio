0b1250e49903b83cb4ca5961ef93fc59db46b733
silviajoy <floriosg@gmail.com>
2019-03-29 10:08:20 +0100

    arm modeling - static generatejointandbone method

--- js/imports/castaway.js
+++ js/imports/castaway.js
@@ -5,7 +5,7 @@ class Castaway  {
      * @param {THREE.Scene} scene 
      */
     constructor(scene) {
-        this.walking = true
+        this.walking = false
         this.ankleDirection = -1 //rendering option
         this.kneeDirection = 1 //rendering option
         this.hipDirection = -1 //rendering option
@@ -20,7 +20,7 @@ class Castaway  {
         this.mainPivot = new THREE.Object3D() 
 
         /* foot modeling snippet*/
-        var ankleGroup = this.generateJointAndBone(unit, this.bodyMaterial, 4, 1, 4, this.bodyMaterial, 4, 2, 6)
+        var ankleGroup = Castaway.generateJointAndBone(unit, this.bodyMaterial, 4, 1, 4, this.bodyMaterial, 4, 2, 6)
 
         ankleGroup.bone.position.y = unit*-1 // positioning foot in height 
         ankleGroup.bone.position.z = unit*1 // positioning foot in depth
@@ -30,7 +30,7 @@ class Castaway  {
         
         /* leg modeling */
         /* lower leg modeling snippet */
-        var lowerLegGroup = this.generateJointAndBone(unit, this.bodyMaterial, 4, 2, 4, this.bodyMaterial, 2, 8, 2)
+        var lowerLegGroup = Castaway.generateJointAndBone(unit, this.bodyMaterial, 4, 2, 4, this.bodyMaterial, 2, 8, 2)
         
         lowerLegGroup.bone.position.y = unit*-4 // positioning lower leg bone
         
@@ -40,7 +40,7 @@ class Castaway  {
         this.pKnee.add(this.pAnkle) // foot depends on knee (move the knee --> move the foot)       
         
         /* upper leg modeling snippet */
-        var upperLegGroup = this.generateJointAndBone(unit, this.pantsMaterial, 4, 4, 4, this.bodyMaterial, 2, 8, 2)
+        var upperLegGroup = Castaway.generateJointAndBone(unit, this.pantsMaterial, 4, 4, 4, this.bodyMaterial, 2, 8, 2)
 
         upperLegGroup.bone.position.y = unit*-4 // positioning upper leg bone
 
@@ -55,7 +55,7 @@ class Castaway  {
         this.pHipR = new THREE.Object3D() // leg pivot - used to move hip
         this.pHipR.add(leg) // entire leg movement depends on this new pivot
 
-        this.pHipR.position.y = unit*14 // positioning leg relative to terrain (sole touch the floor)
+        this.pHipR.position.y = unit*14 // positioning leg relative to terrain (sole touches the floor)
         this.pHipR.position.x = unit*-3
 
 
@@ -73,6 +73,58 @@ class Castaway  {
         this.mainPivot.add(this.pHipR) // mainPivot gains control of legs 
         this.mainPivot.add(this.pHipL)
 
+        
+        /* arm modeling */
+        /* hand modeling snippet */
+        var handGroup = Castaway.generateJointAndBone(unit, this.bodyMaterial, 2, 2, 2, this.bodyMaterial, 4, 1, 2)
+
+        handGroup.bone.position.x = unit*-2 // positioning hand relative to wrist pivot
+
+        var thumb =  new THREE.Mesh(new THREE.BoxGeometry(unit*1, unit*1, unit*1), this.bodyMaterial) //generate box for thumb
+
+        thumb.position.x = unit*-1 // positioning thumb relative to wrist pivot
+        thumb.position.z = unit*1.5
+        
+        this.pHand = handGroup.pivot // wrist pivot - moving the hand
+
+        this.pHand.add(thumb) // add thumb to wrist pivot
+
+        /* lower arm */
+        var lowerArmGroup = Castaway.generateJointAndBone(unit, this.bodyMaterial, 2, 4, 4, this.bodyMaterial, 4, 2, 2)
+
+        lowerArmGroup.bone.position.x = unit*-2 // positioning lower arm bone relative to elbow
+
+        this.pElbow = lowerArmGroup.pivot // elbow pivot - moving lower arm
+
+        this.pHand.position.x = unit*-5 // positioning hand relative to elbow pivot
+
+        this.pElbow.add(this.pHand) // hand pivot is child of elbow pivot
+
+        /* upper arm */
+        var upperArmGroup = Castaway.generateJointAndBone(unit, this.bodyMaterial, 4, 4, 4, this.bodyMaterial, 4, 2, 2)
+
+        upperArmGroup.bone.position.x = unit*-4 // positioning upper arm bone relative to shoulder
+
+        var arm = upperArmGroup.pivot // arm container
+
+        this.pElbow.position.x = unit*-7 // positioning lower arm relative to shoulder pivot
+
+        this.pShoulder = new THREE.Object3D() // shoulder pivot - moving lower arm 
+        arm.add(this.pElbow)   // lower arm is part of arm 
+
+        arm.position.x = unit*-2
+        arm.position.y = unit*2
+
+        this.pShoulder.add(arm) // arm is child of shoulder pivot (move shoulder -> move elbow -> move wrist)
+
+
+        /* add arm to body */
+        this.pShoulder.position.y = unit*21
+        this.pShoulder.position.x = unit*-4
+        this.pShoulder.position.z = unit*-2.5
+
+        this.mainPivot.add(this.pShoulder)
+        
 
         /* body modeling */
         var gBodyLower = new THREE.BoxGeometry(unit*10, unit*4, unit*5)
@@ -93,7 +145,6 @@ class Castaway  {
 
         scene.add(this.mainPivot)
 
-        console.log(this.pHipL)
     }
 
     /**
@@ -110,7 +161,7 @@ class Castaway  {
      * @param {Number} bz - depth of bone box
      * @returns {Object} object containing joint and bone meshes and pivot object3D
      */
-    generateJointAndBone(unit, jMaterial, jx, jy, jz, bMaterial, bx, by, bz) {
+    static generateJointAndBone(unit, jMaterial, jx, jy, jz, bMaterial, bx, by, bz) {
         var pJoint = new THREE.Object3D() //joint pivot
 
         var gBone = new THREE.BoxGeometry(unit*bx, unit*by, unit*bz) // create boxes
@@ -132,7 +183,8 @@ class Castaway  {
       
         return jointAndBone
     }
-
+    
+    /* @TODO more natural animation */ 
     walkAnimation() {
         if(this.pHipR.rotation.x < -30*Math.PI/180) {
             this.hipDirection = 1
@@ -161,8 +213,8 @@ class Castaway  {
 
          
         //this.pAnkle.rotation.x += this.ankleDirection*Math.PI/180 
-        this.pKnee.rotation.x += this.kneeDirection*Math.PI/180
-        this.pHipR.rotation.x += this.hipDirection*Math.PI/180
+        this.pKneeR.rotation.x += this.kneeDirection*Math.PI/180
+        this.pHipR.rotation.z += this.hipDirection*Math.PI/180
 
         //this.pAnkleL.rotation.x += this.ankleDirection*Math.PI/180 
         this.pKneeL.rotation.x += this.kneeDirection*Math.PI/180 
